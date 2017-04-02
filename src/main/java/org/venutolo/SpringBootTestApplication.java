14ecb64a43cf78f8892f7dafe4e80934f16c279e
Rick Venutolo <rick@venutolo.org>
2017-04-02 09:58:04 -0400

    Formatting

--- src/main/java/org/venutolo/SpringBootTestApplication.java
+++ src/main/java/org/venutolo/SpringBootTestApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootTestApplication.class, args);
-	}
+    public static void main(final String... args) {
+        SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
 }
