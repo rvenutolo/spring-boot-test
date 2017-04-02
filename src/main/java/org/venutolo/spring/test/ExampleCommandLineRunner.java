40066a4db9f68cee43333b4dd543c748ae7cd893
Rick Venutolo <rick@venutolo.org>
2017-04-02 14:09:46 -0400

    Add ExampleCommandLineRunner

---
+++ src/main/java/org/venutolo/spring/test/ExampleCommandLineRunner.java
@@ -0,0 +1,33 @@
+package org.venutolo.spring.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class ExampleCommandLineRunner {
+
+    private static final Log logger = LogFactory.getLog(ExampleCommandLineRunner.class);
+
+    public static void main(final String... args) {
+        SpringApplication.run(ExampleCommandLineRunner.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext applicationContext) {
+        return args -> {
+            final String beans = Arrays.stream(applicationContext.getBeanDefinitionNames())
+                                       .sorted()
+                                       .collect(Collectors.joining("\n\t\t", "[\n", "\n]"));
+            logger.info("Spring Boot beans: " + beans);
+        };
+    }
+
+}
