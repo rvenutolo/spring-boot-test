3dfd6c4decd1565bd2a1ffff5bd5a632e3f0d40d
Rick Venutolo <rick@venutolo.org>
2017-04-07 08:40:43 -0400

    Update message keys

--- src/main/java/org/venutolo/spring/test/form/UserRegistrationInputValidator.java
+++ src/main/java/org/venutolo/spring/test/form/UserRegistrationInputValidator.java
@@ -20,15 +20,15 @@ public class UserRegistrationInputValidator implements Validator {
     @Override
     public void validate(final Object target, final Errors errors) {
         final UserRegistrationInput user = (UserRegistrationInput) target;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "error.firstName.empty");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "error.lastName.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "invalid.firstName.empty");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "invalid.lastName.empty");
         try {
             final int ageInt = Integer.parseInt(user.getAge());
             if ((ageInt < MIN_AGE) || (ageInt > MAX_AGE)) {
-                errors.rejectValue("age", "error.age.minMax", new Object[]{MIN_AGE, MAX_AGE}, "");
+                errors.rejectValue("age", "invalid.age.minMax", new Object[]{MIN_AGE, MAX_AGE}, "");
             }
         } catch (final NumberFormatException e) {
-            errors.rejectValue("age", "error.age.notNumber");
+            errors.rejectValue("age", "invalid.age.notNumber");
         }
     }
 
