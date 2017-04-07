7c484d7abce342323e2172a9bce364cf5999c0c5
Rick Venutolo <rick@venutolo.org>
2017-04-07 07:41:25 -0400

    Rename main application class

---
+++ src/main/java/org/venutolo/spring/test/SpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package org.venutolo.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApplication {
+
+    public static void main(final String... args) {
+        SpringApplication.run(SpringBootTestApplication.class, args);
+    }
+
+}
