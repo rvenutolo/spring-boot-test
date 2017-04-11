2a380b6c7c4b65459979993c62d3601aa4a447ab
Rick Venutolo <rick@venutolo.org>
2017-04-11 11:56:59 -0400

    Make existing user registration date random

--- src/main/java/org/venutolo/spring/test/service/InMemoryUserServiceImpl.java
+++ src/main/java/org/venutolo/spring/test/service/InMemoryUserServiceImpl.java
@@ -3,9 +3,11 @@ package org.venutolo.spring.test.service;
 import org.springframework.stereotype.Service;
 import org.venutolo.spring.test.User;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 @Service
@@ -15,11 +17,49 @@ public class InMemoryUserServiceImpl implements UserService {
 
     public InMemoryUserServiceImpl() {
         this.users = new HashSet<>();
-        users.add(new User("Todd", "Bonzalez", 20, 1.28484362498f, true));
-        users.add(new User("Mike", "Truk", 24, 1.549184378531f, true));
-        users.add(new User("Karl", "Dandelton", 34, 1.1684343798f, false));
-        users.add(new User("Raul", "Chamgerlain", 45, 1.781918132f, true));
-        users.add(new User("Glenallen", "Mixon", 54, 1.549751291987f, true));
+        final Random random = new Random();
+        final LocalDate now = LocalDate.now();
+        final int randomIntBound = 30;
+        users.add(new User(
+                "Todd",
+                "Bonzalez",
+                20,
+                1.28484362498f,
+                true,
+                now.minusDays(random.nextInt(randomIntBound))
+        ));
+        users.add(new User(
+                "Mike",
+                "Truk",
+                24,
+                1.549184378531f,
+                true,
+                now.minusDays(random.nextInt(randomIntBound)))
+        );
+        users.add(new User(
+                "Karl",
+                "Dandelton",
+                34,
+                1.1684343798f,
+                false,
+                now.minusDays(random.nextInt(randomIntBound))
+        ));
+        users.add(new User(
+                "Raul",
+                "Chamgerlain",
+                45,
+                1.781918132f,
+                true,
+                now.minusDays(random.nextInt(randomIntBound))
+        ));
+        users.add(new User(
+                "Glenallen",
+                "Mixon",
+                54,
+                1.549751291987f,
+                true,
+                now.minusDays(random.nextInt(randomIntBound))
+        ));
     }
 
     @Override
