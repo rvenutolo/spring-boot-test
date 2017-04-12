0fb6af41d846f3608f8d9574e50cb84d3f6d1958
Rick Venutolo <rick@venutolo.org>
2017-04-12 08:16:00 -0400

    Add StringTrimmerEditor usage

---
+++ src/main/java/org/venutolo/spring/test/form/FormControllerAdvice.java
@@ -0,0 +1,16 @@
+package org.venutolo.spring.test.form;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class FormControllerAdvice {
+
+    @InitBinder
+    public void registerStringTrimmer(final WebDataBinder webDataBinder) {
+        webDataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(false));
+    }
+
+}
