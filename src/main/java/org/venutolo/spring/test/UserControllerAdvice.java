af27a9fafb8bf92bdb053070468dbd55af57f275
Rick Venutolo <rick@venutolo.org>
2017-04-16 15:13:06 -0400

    Move and rename controller advice

--- src/main/java/org/venutolo/spring/test/UserControllerAdvice.java
+++ src/main/java/org/venutolo/spring/test/UserControllerAdvice.java
@@ -1,4 +1,4 @@
-package org.venutolo.spring.test.registration;
+package org.venutolo.spring.test;
 
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.web.bind.WebDataBinder;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
-public class RegistrationControllerAdvice {
+public class UserControllerAdvice {
 
     @InitBinder
     public void registerStringTrimmer(final WebDataBinder webDataBinder) {
